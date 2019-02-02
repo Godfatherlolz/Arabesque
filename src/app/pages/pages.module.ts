6a39bc48a09caa10093f9262af2cdd5a8f00d72f
Khaled Ouertani <khaled.ouertani@esprit.tn>
2019-02-02 19:27:48 +0100

    Pages Module for LayoutTheme
    Using Nebular Components

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+
+import { PagesComponent } from './pages.component';
+import { PagesRoutingModule } from './pages-routing.module';
+import { ThemeModule } from '../@theme/theme.module';
+import { NbCalendarRangeModule, NbSpinnerModule } from '@nebular/theme';
+
+
+
+const PAGES_COMPONENTS = [
+  PagesComponent,
+];
+
+@NgModule({
+  imports: [
+    PagesRoutingModule,
+    ThemeModule,
+    NbCalendarRangeModule,
+    NbSpinnerModule,
+  ],
+  declarations: [
+    ...PAGES_COMPONENTS,
+  ],
+})
+export class PagesModule {
+}
