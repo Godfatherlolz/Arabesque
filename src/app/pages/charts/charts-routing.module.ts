42064c12aa3f0699a98abf96a6eeabfec582d4b5
Khaled Ouertani <khaled.ouertani@esprit.tn>
2019-02-02 20:59:12 +0100

    Charts Routing & Module

--- src/app/pages/charts/charts-routing.module.ts
+++ src/app/pages/charts/charts-routing.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { ChartsComponent } from './charts.component';
+import { D3Component } from './d3/d3.component';
 
 const routes: Routes = [{
   path: '',
   component: ChartsComponent,
+  children: [ {
+    path: 'd3',
+    component: D3Component,
+  }],
 }];
 
 @NgModule({
@@ -16,4 +21,5 @@ export class ChartsRoutingModule { }
 
 export const routedComponents = [
   ChartsComponent,
+  D3Component,
 ];
--- src/app/pages/charts/charts.module.ts
+++ src/app/pages/charts/charts.module.ts
@@ -6,14 +6,23 @@ import { ChartModule } from 'angular2-chartjs';
 import { ThemeModule } from '../../@theme/theme.module';
 
 import { ChartsRoutingModule, routedComponents } from './charts-routing.module';
-
+import { D3BarComponent } from './d3/d3-bar.component';
+import { D3LineComponent } from './d3/d3-line.component';
+import { D3PieComponent } from './d3/d3-pie.component';
+import { D3AdvancedPieComponent } from './d3/d3-advanced-pie.component';
+import { D3BarDatesComponent } from './d3/d3-bar-dates.component';
+import { D3PieCountrySectorComponent } from './d3/d3-pie-country-sector.component';
 
 const components = [
-
+  D3BarComponent,
+  D3LineComponent,
+  D3PieComponent,
+  D3AdvancedPieComponent,
+  D3PieCountrySectorComponent
 ];
 
 @NgModule({
   imports: [ThemeModule, ChartsRoutingModule, NgxEchartsModule, NgxChartsModule, ChartModule],
-  declarations: [...routedComponents, ...components],
+  declarations: [...routedComponents, ...components, D3BarDatesComponent],
 })
 export class ChartsModule {}
